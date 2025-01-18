94fe7d885ec0f9d50c3dcdcc38ede77554648966
Alexandr Iapara <spectrumfenix@yandex.com>
2025-01-18 15:49:01 -0800

    Added imports for node-schedule and pg-promise

--- backend-vav/server.js
+++ backend-vav/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+import pgPromise from "pg-promise";
+import schedule from "node-schedule";
 
 const app = express();
 const PORT = 3000;
@@ -24,3 +27,27 @@ app.get("/config", (req, res) => {
   //send index.html file
   res.sendFile("config.html");
 });
+
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://username:password@host:port/database')
+
+db.one('SELECT $1 AS value', 123)
+  .then((data) => {
+    console.log('DATA:', data.value)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+
+  
+const globalResetJob = schedule.scheduleJob('0 0 * * *', () => {
+  //yourVar = 1;
+});
+
+/* EXAMPLE
+let yourVar = 12345;
+  
+const globalResetJob = schedule.scheduleJob('0 0 * * *', () => {
+  yourVar = 1;
+});
+*/
