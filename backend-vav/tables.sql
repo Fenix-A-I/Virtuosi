0796f0d120a44806b0eb5c095421934664825baa
Jake130 <115515270+Jake130@users.noreply.github.com>
2025-01-18 14:30:22 -0800

    Made SQL file for DB tables

---
+++ backend-vav/tables.sql
@@ -0,0 +1,15 @@
+
+
+CREATE TABLE virtues (
+    habitName VARCHAR,
+    habitIsVirtue BOOLEAN,
+    habitDescription VARCHAR,
+    habitTrackingType VARCHAR,
+    goalDayRequirement VARCHAR,
+    goalDaycountCount INT,
+    goalDaycountPeriod VARCHAR(4,5),
+    goalWeekdaysDays VARCHAR,
+    goalStrakCheat VARCHAR,
+    goalStreakSkipsDays int,
+    goalStreakFreezesAccumulation int
+);
