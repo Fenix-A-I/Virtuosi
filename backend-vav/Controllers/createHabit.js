99f818f49cf297d8f07abc74c38df639cde3a94f
Jake130 <115515270+Jake130@users.noreply.github.com>
2025-01-18 15:46:22 -0800

    createHabit function;Added history table

--- backend-vav/Controllers/createHabit.js
+++ backend-vav/Controllers/createHabit.js
@@ -16,11 +16,18 @@ const createHabit = async(req,res) => {
         if (!habitName || !habitIsVirtue || !habitTrackingType || !goalDayRequirement) {
             return res.status(400).json({ error : "Missing Required Fields"});
         }
+        console.log(goalStreakFreezesAccumulation);
+
         // Are vals set to undefined?
-        const result = pool.query("INSERT INTO virtues VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *",
-            [habitIsVirtue,habitDescription,habitTrackingType,goalDayRequirement,]
+        const result = pool.query("INSERT INTO virtues VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *",    //use pgp
+            [
+                habitIsVirtue,habitDescription,habitTrackingType,goalDayRequirement,goalDaycountCount,goalDaycountPeriod,
+                goalDayWeekdaysDays,goalStreakCheat,goalStreakSkipDays,goalStreakFreezesAccumulation
+            ]
         );
+        return res.status(200).json(result.rows[0]);
     } catch (err) {
-        
+        console.error("Error creating habit", err.message);
+        res.status(500).json({ err:"Internal Server Error"});
     }
 }
--- backend-vav/tables.sql
+++ backend-vav/tables.sql
@@ -1,15 +1,26 @@
 
 
 CREATE TABLE virtues (
-    habitName VARCHAR,
-    habitIsVirtue BOOLEAN,
+    habitName VARCHAR NOT NULL,
+    habitIsVirtue BOOLEAN NOT NULL,
     habitDescription VARCHAR,
-    habitTrackingType VARCHAR,
-    goalDayRequirement VARCHAR,
+    habitTrackingType VARCHAR NOT NULL,
+    goalDayRequirement VARCHAR NOT NULL,
     goalDaycountCount INT,
     goalDaycountPeriod VARCHAR(4,5),
     goalWeekdaysDays VARCHAR,
     goalStrakCheat VARCHAR,
     goalStreakSkipsDays int,
     goalStreakFreezesAccumulation int
+);
+
+CREATE TABLE history (
+    habitName VARCHAR NOT NULL,
+    habitIsVirtue BOOLEAN NOT NULL,
+    habitTrackingType NOT NULL,
+    goalDayRequirement NOT NULL,
+    count int,
+    completed BOOLEAN,
+    timeperiod decimal(10,2),
+    dayRecorded date,
 );
