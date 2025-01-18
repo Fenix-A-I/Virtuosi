33b9ad64ac1c272e9a08b641f737b966624e34e0
Alexandr Iapara <spectrumfenix@yandex.com>
2025-01-18 14:58:11 -0800

    Added new functions in habits.js

--- backend-vav/Routers/habits.js
+++ backend-vav/Routers/habits.js
@@ -2,5 +2,15 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/create-new-habit', createHabit);     // createHabit will be a function in the Controllers directory
+router.delete('/delete-habit', deleteHabit);
+router.put('/update-habit', updateHabit);
+router.get('/get-habit', getHabit);
+router.get('/get-habits-list', getHabits);
+router.put('/update-habit-name', updateHabitName);
+
+router.post('/goalday', goalday);
+router.put('/update-goal', updateGoal);
+router.put('/update-streak', updateStreak);
+
 
 export default router;
