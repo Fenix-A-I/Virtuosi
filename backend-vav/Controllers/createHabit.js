88fab8cbf15b1525d20e0fbc6bb686e33e32ae1c
Jake130 <115515270+Jake130@users.noreply.github.com>
2025-01-18 18:50:05 -0800

    Finished create habit func

--- backend-vav/Controllers/createHabit.js
+++ backend-vav/Controllers/createHabit.js
@@ -1,10 +1,11 @@
+import { isUndefined } from 'util';
 import { pool } from '../db.js';
 
 const createHabit = async(req,res) => {
     try {
         const {habitName,
             habitIsVirtue,
-            habitDescription, 
+            habitDescription,
             habitTrackingType,
             goalDayRequirement,
             goalDaycountCount,
@@ -16,13 +17,22 @@ const createHabit = async(req,res) => {
         if (!habitName || !habitIsVirtue || !habitTrackingType || !goalDayRequirement) {
             return res.status(400).json({ error : "Missing Required Fields"});
         }
-        console.log(goalStreakFreezesAccumulation);
+        
+        const newHabitDesc = habitDescription === undefined ? "a" : habitDescription;
+        const newGoalDaycountCount = goalDaycountCount === undefined ? null : goalDaycountCount;
+        const newGoalDaycountPeriod = goalDaycountPeriod === undefined ? null: goalDaycountPeriod;
+        const newGoalDayWeekdaysDays = goalDayWeekdaysDays === undefined ? null: goalDayWeekdaysDays;
+        const newGoalStreakCheat = goalStreakCheat === undefined ? null: goalStreakCheat;
+        const newGoalStreakSkipDays = goalStreakSkipDays === undefined ? null: goalStreakSkipDays;
+        const newGoalStreakFreezesAccumulation = goalStreakFreezesAccumulation === undefined ? null: goalStreakFreezesAccumulation;
 
+        //console.log(habitName,habitIsVirtue,newHabitDesc,habitTrackingType,goalDayRequirement,newGoalDaycountCount,newGoalDaycountPeriod,
+        //    newGoalDayWeekdaysDays,newGoalStreakCheat,newGoalStreakSkipDays,newGoalStreakFreezesAccumulation);
         // Are vals set to undefined?
-        const result = pool.query("INSERT INTO virtues VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *",    //use pgp
+        const result = await pool.query("INSERT INTO virtues VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *",    //use pgp
             [
-                habitIsVirtue,habitDescription,habitTrackingType,goalDayRequirement,goalDaycountCount,goalDaycountPeriod,
-                goalDayWeekdaysDays,goalStreakCheat,goalStreakSkipDays,goalStreakFreezesAccumulation
+                habitName,habitIsVirtue,newHabitDesc,habitTrackingType,goalDayRequirement,newGoalDaycountCount,newGoalDaycountPeriod,
+                newGoalDayWeekdaysDays,newGoalStreakCheat,newGoalStreakSkipDays,newGoalStreakFreezesAccumulation
             ]
         );
         return res.status(200).json(result.rows[0]);
